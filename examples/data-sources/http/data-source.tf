14c3cd12f873644a97f130a8d1a41d25670cbce5
Mehdi Souihed <mehdi.souihed@thisisbud.com>
2022-07-14 14:27:27 +0100

    Update Readme and cleanup

--- examples/data-sources/http/data-source.tf
+++ examples/data-sources/http/data-source.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     http = {
       source  = "MehdiAtBud/http"
-      version = "2.2.15"
+      version = "2.2.16"
     }
   }
 }
@@ -10,7 +10,7 @@ terraform {
 # The following example shows how to issue an HTTP GET request supplying
 # an optional request header.
 data "http" "example" {
-  url = "https://checkpoin-api.hashicorp.com/v1/check/terraform"
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
 
   # Optional request headers
   request_headers = {
@@ -19,13 +19,6 @@ data "http" "example" {
 
   max_elapsed_time = 10
   initial_interval = 100
-  multiplier       = 1.2
-  max_interval     = 500000000000000000
-}
-
-
-
-
-provider "http" {
-  # Configuration options
+  multiplier       = "1.2"
+  max_interval     = 50000
 }
